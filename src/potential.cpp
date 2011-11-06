c1717a6166473ceee82ace3ec81ecd1313085d45
monson <holymonson@gmail.com>
2011-11-06 21:41:19 +0800

    temp

--- src/potential.cpp
+++ src/potential.cpp
@@ -11,7 +11,7 @@ static const double epsilon_0 = GSL_CONST_MKSA_VACUUM_PERMITTIVITY;
 static const double epsilon_0_4_pi = epsilon_0 * 4*M_PI;
 
 
-double Potential_base_metal_ball::operator() (double x) const
+double Potential_metal_ball::operator() (double x) const
 /* distance from origin */
 {
     assert(x >= 0);
--- src/potential.h
+++ src/potential.h
@@ -3,79 +3,42 @@
 
 #include <cassert>
 #include <vector>
-#include <memory>
 
 #include <gsl/gsl_math.h>
 
 #define hypot(x,y,z) gsl_hypot3(x,y,z)
 
 
-class Potential_base
-{
-    public:
-        virtual double operator() (double x) const = 0;
-};
-
-typedef std::shared_ptr<Potential_base> Shared_ptr_Potential_base;
-
 class Potential
 {
     public:
-        virtual double operator() (double x) const {return -1e100;}
-//        {
-//            return (*_pbase)(x);
-//        }
-//    public:
-//        Shared_ptr_Potential_base _pbase;
+        virtual double operator() (double x)  const {}// = 0;
 };
 
 /* metal ball potential */
-class Potential_base_metal_ball: public Potential_base
+class Potential_metal_ball: public Potential
 {
     public:
-        Potential_base_metal_ball(double Q, double R)
+        Potential_metal_ball(double Q, double R)
             : _Q(Q), _R(R) { assert(R > 0); }
         double operator() (double x) const;
     private:
         double _Q;
         double _R;
 };
-class Potential_metal_ball: public Potential
-{
-    public:
-        typedef Potential_base_metal_ball Base;
-        Potential_metal_ball(double Q, double R)
-            : _pbase(new Base(Q, R)) {}
-        double operator() (double x) const
-        {   return (*_pbase)(x); }
-    private:
-        std::shared_ptr<Base> _pbase;
-};
 
 /* boost potential */
-class Potential_base_boost: public Potential_base
-{
-    public:
-        Potential_base_boost(const Potential& phi, double x0)
-            : _pphi(new Potential(phi)), _x0(x0) {}
-        double operator() (double x) const
-        {
-            return (*_pphi)(x-_x0);
-        }
-    private:
-        Potential* _pphi;
-        double _x0;
-};
 class Potential_boost: public Potential
 {
     public:
-        typedef Potential_base_boost Base;
+        typedef Potential_boost Base;
         Potential_boost(const Potential& phi, double x0)
-            : _pbase(new Base(phi, x0)) {}
+            : _phi(phi), _x0(x0) {}
         double operator() (double x) const
-        {   return (*_pbase)(x); }
+        {   return _phi(x-_x0); }
     private:
-        std::shared_ptr<Base> _pbase;
+        const Potential _phi;
+        double _x0;
 };
 
 
@@ -83,74 +46,45 @@ class Potential_boost: public Potential
  * potential in 3d
  */
 
-class Potential_base_3d
-{
-    public:
-        virtual double operator() (double x, double y, double z) const = 0;
-};
-
-typedef std::shared_ptr<Potential_base_3d> Shared_ptr_Potential_base_3d;
-
 class Potential_3d
 {
     public:
-        double operator() (double x, double y, double z) const
-        {
-            return (*_pbase)(x, y, z);
-        }
-    protected:
-        Shared_ptr_Potential_base_3d _pbase;
+        virtual double operator() (double x, double y, double z) const  {}//= 0;
 };
 
 /* metal ball potential in 3d*/
-class Potential_base_metal_ball_3d: public Potential_base_3d
-{
-    public:
-        Potential_base_metal_ball_3d(double Q, double R)
-            : _phi(Q, R) {}
-        double operator() (double x, double y, double z) const
-        {
-            return _phi(hypot(x, y, z));
-        }
-    private:
-        Potential_metal_ball _phi;
-};
 class Potential_metal_ball_3d: public Potential_3d
 {
     public:
         Potential_metal_ball_3d(double Q, double R)
-            : _pbase(new Potential_base_metal_ball_3d(Q, R)) {}
-        
-    private:
-        Shared_ptr_Potential_base_3d _pbase;
+            : _phi(Q, R) {}   
+        double operator() (double x, double y, double z) const
+        {
+            return _phi(hypot(x, y, z));
+        }
+    private:    
+        Potential_metal_ball _phi;
 };
 
 /*  boost potential in 3d */
-class Potential_base_boost_3d: public Potential_base_3d
+class Potential_boost_3d: public Potential_3d
 {
     public:
-        Potential_base_boost_3d(const Potential_3d& phi, double x0, double y0, double z0)
+        Potential_boost_3d(const Potential_3d& phi, double x0, double y0, double z0)
             : _phi(phi), _x0(x0), _y0(y0), _z0(z0) {}
         double operator()(double x, double y, double z) const
         {
             return _phi(x-_x0, y-_y0, z-_z0);
         }
     private:
-        const Potential_3d _phi;
-        double _x0, _y0, _z0;
-};
-class Potential_boost_3d: public Potential_3d
-{
-    public:
-        Potential_boost_3d(const Potential_3d& phi, double x0, double y0, double z0)
-            : _pbase(new Potential_base_boost_3d(phi, x0, y0, z0)) {}
-    private:
-        Shared_ptr_Potential_base_3d _pbase;        
+        Potential_3d _phi;
+        double _x0, _y0, _z0;    
 };
 
 /*  superimpose potential in 3d */
-template <class Container>
-class Potential_base_superimpose_3d: public Potential_base_3d, public Container
+/*
+template <class Container = std::vector<const Potential_3d>>
+class Potential_superimpose_3d: public Potential_3d, public Container
 {
     public:
         double operator()(double x, double y, double z) const
@@ -161,43 +95,18 @@ class Potential_base_superimpose_3d: public Potential_base_3d, public Container
             return sum;
         }
 };
-template <class Container = std::vector<const Potential_3d> >
-class Potential_superimpose_3d: public Potential_3d
-{
-    public:
-        typedef Potential_base_superimpose_3d<Container> Base;
-//        Potential_superimpose_3d(): _pbase(new Base) {}
-//        void push_back(const Potential_3d& phi)
-//        {
-//            _pbase->push_back(phi);
-//        }
-    private:
-        std::shared_ptr<Base> _pbase;
-};
-
-class Potential_energy_base
-{
-    public:
-        Potential_energy_base(const Potential& phi, double phi0): _pphi(new Potential(phi)), _phi0(phi0) {}
-        double operator() (double x) const
-        {
-            return - ((*_pphi)(x)-_phi0);
-        }
-    private:
-        const Potential* _pphi;
-        double _phi0;
-};
+*/
 class Potential_energy
 {
     public:
-        typedef Potential_energy_base Base;
         Potential_energy(const Potential& phi, double phi0)
-            : _pbase(new Base(phi, phi0)) {}
+            : _phi(phi), _phi0(phi0) {}
         double operator() (double x) const
         {
-            return (*_pbase)(x);
+            return - (_phi(x)-_phi0);
         }
     private:
-        std::shared_ptr<Base> _pbase;
+        Potential _phi;
+        double _phi0;
 };
 #endif // _POTENTIAL_H_
