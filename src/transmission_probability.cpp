a6fa0663472cc3706fe0a9e1ae2e95a9acf5bb66
monson <holymonson@gmail.com>
2011-11-18 10:08:39 +0800

    fix integrate singularity

--- src/transmission_probability.cpp
+++ src/transmission_probability.cpp
@@ -14,7 +14,7 @@ static constexpr double epsabs = 0;
 static constexpr double epsrel = 1e-8;
 static constexpr size_t iter_limit = 100;
 static constexpr size_t integration_workspace_size = 1024;
-static constexpr int integ_key = GSL_INTEG_GAUSS41;
+//static constexpr int integ_key = GSL_INTEG_GAUSS41;
 
 static constexpr double m_e = GSL_CONST_MKSA_MASS_ELECTRON;
 static constexpr double hbar = GSL_CONST_MKSA_PLANCKS_CONSTANT_HBAR;
@@ -31,7 +31,7 @@ static inline double int_func(double x, void* int_params)
     const Potential_energy* pU;
     pU = static_cast<int_params_struct*>(int_params)->pU;
     double E = static_cast<int_params_struct*>(int_params)->E;
-    double U_x = (*pU)(x); 
+    double U_x = (*pU)(x);
     if (E >= U_x)
     {   return 0; }
     else
@@ -49,8 +49,8 @@ double Transmission_probability::operator() (double E) const
     f.function = &int_func;
     f.params = &int_params;
     double result, abserr;
-    gsl_integration_qag(&f, 0, xmax, epsabs, epsrel, iter_limit,
-        integ_key, w, &result, &abserr);        
+    gsl_integration_qags(&f, 0, xmax, epsabs, epsrel, iter_limit,
+        /*integ_key,*/ w, &result, &abserr);
     gsl_integration_workspace_free(w);
     if (result == 0)
     {  return 1; }
